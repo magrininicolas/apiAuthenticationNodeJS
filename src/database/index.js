861a69c8ab792ab40a9148b7aa9fb31556c95e43
Nicolas <magrininicolas@outlook.com>
2023-12-03 11:59:23 -0300

    Alterações de segurança

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const dbURI = "mongodb+srv://nicolas:@mongo-api-nodejs.s17cqiy.mongodb.net/?retryWrites=true&w=majority";
+const dbURI = `mongodb+srv://nicolas:${process.env.DB_PASSWORD}@mongo-api-nodejs.s17cqiy.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose.connect(dbURI);
 
